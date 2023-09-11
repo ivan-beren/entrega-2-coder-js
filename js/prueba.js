17cd64694d66f23b05c9c45699eaeda873208d32
Ivan Berengeno <ivan.beren.mdp@gmail.com>
2023-09-11 20:51:53 -0300

    Se agregan color a los textos informativos

--- js/prueba.js
+++ js/prueba.js
@@ -16,10 +16,14 @@
             //pileta, se aplicaria para un segundo ingreso de materia prima
             if (volumen <= this.capacidad && volumen <= (this.capacidad-this.volumenActual)){
             this.volumenActual += volumen
-            infoPileta.innerHTML = (`Se ingresan ${volumen} kilos de pescado a la pileta`)
+            infoPileta.innerHTML = `Se ingresan ${volumen} kilos de pescado a la pileta`
+            infoPileta.classList.remove('infoRojo')
+            infoPileta.classList.add('infoVerde')
             }else{
             //En cualquiera de los dos casos, se superaria la capacidad y no ingresaria esa cantidad de pescado
-            infoPileta.innerHTML = (`Se alcanzo la capacidad maxima de pescado en la pileta`)
+            infoPileta.innerHTML = `Se alcanzo la capacidad maxima de pescado en la pileta`
+            infoPileta.classList.remove('infoVerde')
+            infoPileta.classList.add('infoRojo')
             }
         },
         transferirPescado: function(volumen){
@@ -27,14 +31,23 @@
                 //Este if transfiere materia prima si se cumplen 3 condiciones: 1ro, que el vol
                 this.volumenActual -= volumen
                 cocinador.volumenCrudo += volumen
-                infoPileta.innerHTML = (`Se transfirieron ${volumen} kilos de pescado al cocinador`)
+                infoPileta.innerHTML = `Se transfirieron ${volumen} kilos de pescado al cocinador`
+                infoCocinador.innerHTML = `Hay ${cocinador.volumenCrudo}`
+                infoPileta.classList.remove('infoRojo')
+                infoPileta.classList.add('infoVerde')
             }else if(this.volumenActual == 0){
-                infoPileta.innerHTML = (`No se puede transferir porque no hay pescado en pileta`)
+                infoPileta.innerHTML = `No se puede transferir porque no hay pescado en pileta`
+                infoPileta.classList.remove('infoVerde')
+                infoPileta.classList.add('infoRojo')
             }else if(volumen > this.volumenActual){
                 //Este if me avisa si quiero transferir mas cantidad de la que hay en la pileta
-                infoPileta.innerHTML = (`Se quieren trasnferir ${volumen} kilos de pescado, pero solo hay disponibles ${this.volumenActual}`)
+                infoPileta.innerHTML = `Se quieren trasnferir ${volumen} kilos de pescado, pero solo hay disponibles ${this.volumenActual}`
+                infoPileta.classList.remove('infoVerde')
+                infoPileta.classList.add('infoRojo')
             }else{
-                infoPileta.innerHTML = (`La cantidad de pescado que quiere transferir supera la capacidad maxima del cocinador`)
+                infoPileta.innerHTML = `La cantidad de pescado que quiere transferir supera la capacidad maxima del cocinador`
+                infoPileta.classList.remove('infoVerde')
+                infoPileta.classList.add('infoRojo')
             }
         },
     }
@@ -50,25 +63,39 @@
             if (volumen > 0 && volumen <= this.volumenCrudo){
                 this.volumenCrudo -= volumen
                 this.volumenCocinado += volumen
-                infoCocinador.innerHTML = (`Se cocinaron ${volumen} de pescado crudo`)
+                infoCocinador.innerHTML = `Se cocinaron ${volumen} de pescado crudo`
+                infoCocinador.classList.remove('infoRojo')
+                infoCocinador.classList.add('infoVerde')
             }else if(volumen == 0){
-                infoCocinador.innerHTML = (`No hay pescado crudo para cocinar`)
+                infoCocinador.innerHTML = `No hay pescado crudo para cocinar`
+                infoCocinador.classList.remove('infoVerde')
+                infoCocinador.classList.add('infoRojo')
             }else{
-                infoCocinador.innerHTML = (`Se quieren cocinar ${volumen} kilos de pescado pero solo hay disponible ${this.volumenCrudo}`)
+                infoCocinador.innerHTML = `Se quieren cocinar ${volumen} kilos de pescado pero solo hay disponible ${this.volumenCrudo}`
+                infoCocinador.classList.remove('infoVerde')
+                infoCocinador.classList.add('infoRojo')
             }
         },
         transferirPescado: function(volumen){
             if (this.volumenCocinado > 0 && volumen <= this.volumenCocinado && volumen <= (prensa.capacidad-(prensa.volumenActual + prensa.volumenPrensado))){
                 this.volumenCocinado -= volumen
                 prensa.volumenActual += volumen
-                infoCocinador.innerHTML = (`Se transfieren ${volumen} de pescado cocinado a la prensa`)
+                infoCocinador.innerHTML = `Se transfieren ${volumen} de pescado cocinado a la prensa`
+                infoCocinador.classList.remove('infoRojo')
+                infoCocinador.classList.add('infoVerde')
             }else{
                 if (this.volumenCocinado == 0){
-                    infoCocinador.innerHTML = (`No se puede transferir porque no hay pescado cocinado`)
+                    infoCocinador.innerHTML = `No se puede transferir porque no hay pescado cocinado`
+                    infoCocinador.classList.remove('infoVerde')
+                    infoCocinador.classList.add('infoRojo')
                 }else if (volumen > this.volumenCocinado){
-                    infoCocinador.innerHTML = (`Usted quiso transferir ${volumen} kilos de pescado pero solo hay disponible ${this.volumenCocinado}`)
+                    infoCocinador.innerHTML = `Usted quiso transferir ${volumen} kilos de pescado pero solo hay disponible ${this.volumenCocinado}`
+                    infoCocinador.classList.remove('infoVerde')
+                    infoCocinador.classList.add('infoRojo')
                 }else{
-                    infoCocinador.innerHTML = (`La cantidad de pescado cocinado que quiere transferir supera la capacidad maxima de la prensa`)
+                    infoCocinador.innerHTML = `La cantidad de pescado cocinado que quiere transferir supera la capacidad maxima de la prensa`
+                    infoCocinador.classList.remove('infoVerde')
+                    infoCocinador.classList.add('infoRojo')
                 }
             }
         },
@@ -82,30 +109,42 @@
         capacidad: 2000,
         volumenActual: 0,
         volumenPrensado: 0,
-        temperatura: 0,
-        rodamiento: 0,
         prensarPescado: function(volumen){
             if (volumen > 0 && volumen <= this.volumenActual){
                 this.volumenActual -= volumen
                 this.volumenPrensado += volumen
-                infoPrensa.innerHTML = (`Se prensan ${volumen} de pescado previamente cocinado`)
+                infoPrensa.innerHTML = `Se prensan ${volumen} de pescado previamente cocinado`
+                infoPrensa.classList.remove('infoRojo')
+                infoPrensa.classList.add('infoVerde')
             }else if(volumen == 0){
-                infoPrensa.innerHTML = ("No hay pescado para prensar")
+                infoPrensa.innerHTML = "No hay pescado para prensar"
+                infoPrensa.classList.remove('infoVerde')
+                infoPrensa.classList.add('infoRojo')
             }else{
-                infoPrensa.innerHTML = ("Se deja de ingresar pescado por equipo sobrecargado")
+                infoPrensa.innerHTML = "Se deja de ingresar pescado por equipo sobrecargado"
+                infoPrensa.classList.remove('infoVerde')
+                infoPrensa.classList.add('infoRojo')
             }
         },
         transferirPrensado: function(volumen){
             if (volumen > 0 && volumen <= this.volumenPrensado && volumen <= (rtd.capacidad-(rtd.volumenActual + rtd.volumenSecado))){
                 this.volumenPrensado -= volumen
                 rtd.volumenActual += volumen
-                infoPrensa.innerHTML = (`Se transfieren ${volumen} kilos de prensado al rotadisco`)
+                infoPrensa.innerHTML = `Se transfieren ${volumen} kilos de prensado al rotadisco`
+                infoPrensa.classList.remove('infoRojo')
+                infoPrensa.classList.add('infoVerde')
             }else if(volumen == 0){
-                infoPrensa.innerHTML = (`No hay pescado prensado`)
+                infoPrensa.innerHTML = `No hay pescado prensado`
+                infoPrensa.classList.remove('infoVerde')
+                infoPrensa.classList.add('infoRojo')
             }else if(volumen > this.volumenPrensado){
-                infoPrensa.innerHTML = (`Se quieren transferir ${volumen} kilos de prensado pero solo hay ${this.volumenPrensado} disponibles`)
+                infoPrensa.innerHTML = `Se quieren transferir ${volumen} kilos de prensado pero solo hay ${this.volumenPrensado} disponibles`
+                infoPrensa.classList.remove('infoVerde')
+                infoPrensa.classList.add('infoRojo')
             }else{
-                infoPrensa.innerHTML = (`La cantidad de prensado que quiere transferir supera la capacidad maxima del rotadisco`)
+                infoPrensa.innerHTML = `La cantidad de prensado que quiere transferir supera la capacidad maxima del rotadisco`
+                infoPrensa.classList.remove('infoVerde')
+                infoPrensa.classList.add('infoRojo')
             }
         },
     }
@@ -118,30 +157,36 @@
         capacidad: 2000,
         volumenActual: 0,
         volumenSecado: 0,
-        temperatura: 0,
-        amperaje: 0,
         secarPrensado: function(volumen){
             if (volumen > 0 && volumen <= this.volumenActual){
                 this.volumenActual -= volumen
                 this.volumenSecado += volumen
-                infoRtd.innerHTML = (`Se secan ${volumen} kilos del prensado ingresado al rotadisco`)
+                infoRtd.innerHTML = `Se secan ${volumen} kilos pescado previamente cocinado y prensado`
+                infoRtd.classList.remove('infoRojo')
+                infoRtd.classList.add('infoVerde')
             }else if(volumen == 0){
-                infoRtd.innerHTML = ("El rotadisco se encuentra vacio")
+                infoRtd.innerHTML = "El rotadisco se encuentra vacio"
+                infoRtd.classList.remove('infoVerde')
+                infoRtd.classList.add('infoRojo')
             }else{
-                infoRtd.innerHTML = ("Se deja de ingresar prensado por equipo sobrecargado")
+                infoRtd.innerHTML = "Se deja de ingresar prensado por equipo sobrecargado"
+                infoRtd.classList.remove('infoVerde')
+                infoRtd.classList.add('infoRojo')
             }
         },
         trasnferirHarina: function(volumen){
             if(volumen > 0 && volumen <= this.volumenSecado && volumen <= (silo.capacidad - silo.volumenActual)){
                 this.volumenSecado -= volumen
                 silo.volumenActual += volumen
-                infoRtd.innerHTML = (`Se transfieren ${volumen} kilos de harina al silo`)
+                infoRtd.innerHTML = `Se transfieren ${volumen} kilos de harina al silo`
             }else if(volumen == 0){
-                colsole.log(`El silo se encuentra vacio`)
+                infoRtd.innerHTML = `El Rotadisco se encuentra vacio`
+                infoRtd.classList.remove('infoVerde')
+                infoRtd.classList.add('infoRojo')
             }else if(volumen > this.volumenSecado){
-                infoRtd.innerHTML = (`Se quieren transferir ${volumen} kilos de harina pero solo hay ${this.volumenSecado} disponibles`)
+                infoRtd.innerHTML = `Se quieren transferir ${volumen} kilos de harina pero solo hay ${this.volumenSecado} disponibles`
             }else{
-                infoRtd.innerHTML = (`La cantidad de prensado que quiere transferir supera la capacidad maxima del silo`)
+                infoRtd.innerHTML = `La cantidad de harina que quiere transferir supera la capacidad maxima del silo`
             }
             
         },
@@ -157,14 +202,20 @@
         volumenActual: 0,
         averiguarBolsas: function(){
             let bolsasDisponibles = Math.floor(this.volumenActual / 50)
-            infoSilo.innerHTML = (`La cantidad de bolsas disponibles es ${bolsasDisponibles}`)
+            infoSilo.innerHTML = `La cantidad de bolsas disponibles es ${bolsasDisponibles}`
             return bolsasDisponibles
         },
         sacarBolsas: function(cantidadBolsas){
             if(cantidadBolsas <= Math.floor(this.volumenActual / 50)){
                 this.volumenActual -= cantidadBolsas * 50
                 organizadorLote.bolsasTotales += cantidadBolsas
-                infoSilo.innerHTML = (`Se embolsaron ${cantidadBolsas} bolsas de 50 kilos`)
+                infoSilo.innerHTML = `Se embolsaron ${cantidadBolsas} bolsas de 50 kilos`
+                infoSilo.classList.remove('infoRojo')
+                infoSilo.classList.add('infoVerde')
+            }else{
+                infoSilo.innerHTML = `No hay harina para embolsar bolsas`
+                infoSilo.classList.remove('infoVerde')
+                infoSilo.classList.add('infoRojo')
             }
         },
     }
@@ -181,10 +232,8 @@
                 for(let i = 0; i < lotesDisponibles; i++){
                     Lotes.push(`Lote numero ${Lotes.length + 1}`)
                 }
-            infoLote.innerHTML = (`Se crean ${Lotes.length} Lotes`)
+            infoLote.innerHTML = `Se crean ${Lotes.length} Lotes`
             }
-            // Se guardan las bolsas totales en localStorage
-            localStorage.setItem('bolsasTotales', this.bolsasTotales);
         },
     }
 
@@ -347,17 +396,46 @@ btnSacarBolsas.addEventListener('click', sacarBolsaSilo)
 let bolsasDisponibles = document.getElementById('bolsasDisponibles')
 let pilasDisponibles = document.getElementById('pilasDisponibles')
 let btnCrearLote = document.getElementById('btnCrearLote')
+let btnVerLote = document.getElementById('btnVerLote')
 let infoLote = document.getElementById('infoLote')
 
-const storedBolsasTotales = localStorage.getItem('bolsasTotales');
-    if (storedBolsasTotales) {
-        organizadorLote.bolsasTotales = parseInt(storedBolsasTotales);
-}
 
 function funCrearLote () {
     organizadorLote.crearLote()
+//primero se guarda el objeto como un string, pasa a ser tecxto plano, se pasa con JSON.stringify(nombre del objeto)
+    let loteString = JSON.stringify(Lotes)
+//segundo se manda el objeto transformado en string al local storage con localStorage.setItem(nombre del objeto string)
+    localStorage.setItem('pilas', loteString)
     bolsasDisponibles.innerHTML = `Bolsas Disponibles: <strong>${organizadorLote.bolsasTotales} bolsas</strong>`
     pilasDisponibles.innerHTML = `Cantidad de Lotes: <strong>${Lotes.length}</strong>`
 }
 
+function funVerLote () {
+//tercero se trae el string del local storage con localStorage.getItem(nombre del objeto string)
+    let pilaLocalStorage = localStorage.getItem('pilas')
+//cuarto se convierte el string en un objeto nuevamente con JSON.parse(nombre del string)
+    let pilaParseada = JSON.parse(pilaLocalStorage)
+    console.log(pilaParseada)
+}
+
 btnCrearLote.addEventListener('click', funCrearLote)
+btnVerLote.addEventListener('click', funVerLote)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
